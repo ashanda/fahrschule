33e7d52601750a65133dd89c5118f18c49896c4a
Ashan Devinda <37576256+ashanda@users.noreply.github.com>
2022-05-24 12:15:28 +0530

    Empty Shop page redirect home

--- wp-content/themes/fahrschule/functions.php
+++ wp-content/themes/fahrschule/functions.php
@@ -213,6 +213,7 @@ add_filter( 'show_admin_bar', '__return_false' );
 		  
 	}
 
+	add_filter( 'woocommerce_return_to_shop_redirect', 'custom_empty_cart_redirect_url' );
 	function custom_empty_cart_redirect_url(){
 
 		return home_url();
