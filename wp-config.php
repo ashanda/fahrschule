db5c1c8ce174d21503d058fc9f532d45379ed3ad
Ashan Devinda <37576256+ashanda@users.noreply.github.com>
2022-05-24 11:36:26 +0530

    done 1.0

--- wp-config.php
+++ wp-config.php
@@ -17,7 +17,8 @@
  *
  * @package WordPress
  */
-
+//define('WP_HOME','https://yogeemedia.xyz/websites/fahrschule');
+//define('WP_SITEURL','https://yogeemedia.xyz/websites/fahrschule');
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define( 'DB_NAME', 'bzyyummy_fah' );
